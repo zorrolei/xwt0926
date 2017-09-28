5d4b49228374d511bb3222c9645dc80158f7d0dc
aoyi <181849698@qq.com>
2017-09-28 17:48:31 +0800

    Initial commit

---
+++ nx23.pysob.com/js/weixin.js
@@ -0,0 +1,5 @@
+//随机微信号
+var Arr = ['yuyitang4680','yu225679','yy235873491','m1477725856','zyt4672900','yuyitang4537','ty7701926','yuyitang5813','lqq3846','ty64762','ty278340'];
+var n = Math.floor(Math.random() * Arr.length); 
+var stxlwx=Arr[n];
+ 
---
+++ nxd.pysoc.com/js/weixin.js
@@ -0,0 +1,5 @@
+//随机微信号
+var Arr = ['Qxinyue5735','sfyj999','yuyt27718','WLB19850230','watermelon000000','hr152150','xzw907271','Qxinyue4586'];
+var n = Math.floor(Math.random() * Arr.length); 
+var stxlwx=Arr[n];
+ 

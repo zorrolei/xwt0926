642b672ef3243cc1bf8f713238bee05b77dddfa7
aoyi <181849698@qq.com>
2017-09-29 11:00:23 +0800

    修改微信号

--- syy.wkcg4.com/js/weixin.js
+++ syy.wkcg4.com/js/weixin.js
@@ -1,5 +1,5 @@
 //随机微信号
-var Arr = ['Yuyitang633','K31280','yuyt30825','Qxinyue0131','yuyitang0960','ty297953','yuyitang6829','yt573426','g131299','Yuyitang7658','yuyt44079','t417227693',' ty493027'];
+var Arr = ['ty7343','yuyitang0960','ty493027','yuyt30825','yuyt44079','Yuyitang9573','Yuyitang7658','Qxinyue0131','yuyitang6829','k31280','g131299','Yuyitang633'];
 var n = Math.floor(Math.random() * Arr.length); 
 var stxlwx=Arr[n];
  

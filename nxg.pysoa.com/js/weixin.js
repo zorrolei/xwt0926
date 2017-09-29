5eacfbc4bb2ca450c20b1758bbe4e9aa34b8b1f0
aoyi <181849698@qq.com>
2017-09-29 21:52:38 +0800

    添加uxt.pysob.com

--- nxg.pysoa.com/js/weixin.js
+++ nxg.pysoa.com/js/weixin.js
@@ -1,5 +1,5 @@
 //随机微信号
-var Arr = ['yuyitang4680','yu225679','yy235873491','m1477725856','zyt4672900','yuyitang4537','ty7701926','yuyitang5813','lqq3846','ty64762','yuyitang1719'];
+var Arr = ['yuyitang4680','yu225679','yy235873491','m1477725856','zyt4672900','yuyitang4537','ty7701926','yuyitang5813','ty64762','yuyitang1719'];
 var n = Math.floor(Math.random() * Arr.length); 
 var stxlwx=Arr[n];
  
